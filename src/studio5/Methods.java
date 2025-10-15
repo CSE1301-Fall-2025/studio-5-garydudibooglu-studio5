7cda9d1d60c402b83ed3e6765c7be1a7fe9a5ce0
Alexander Du <du.a@wustl.edu>
2025-10-15 13:50:20 -0500

    1

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -63,7 +63,7 @@ public class Methods {
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
 		// TODO: Finish this method
-		
+		result = source.replace(Character.toString(target), replacement);
 		return result;
 	}
 
@@ -75,8 +75,9 @@ public class Methods {
 	 */
 	public static int arraySum(int[] values) {
 		int sum = 0;
-		// FIXME: Compute the sum of the values in an array
-		
+		for (int i = 0; i < values.length; i++) {
+			sum = sum + values[i];
+		}		
 		return sum;
 	}
 
@@ -88,16 +89,19 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
-		// TODO: Finish this method
-
-		
-
+		int[] values = new int[length]; 
+		for (int i = 0; i < length; i++) {
+			values[i] = value;
+		}
 		return values;
 	}
 
 	// TODO: Create an arrayMean method which accepts an int array of values parameter.
 	// TODO: Create a JavaDoc comment for the arrayMean method.
-
+	public static double arrayMean(int[] values) {
+		double mean = 0;
+		mean = (double) arraySum(values) / values.length;
+		return mean;
+	}
 	
 }
